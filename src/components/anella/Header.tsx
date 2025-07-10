c9ded0e7bb2bb08a48dd2f48a4402dde4d8fc734
Zeta Leyva <zetaleyva2045@gmail.com>
2025-07-10 16:53:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/anella/Header.tsx
+++ src/components/anella/Header.tsx
@@ -238,11 +238,12 @@ export function Header() {
 const ListItem = React.forwardRef<
   React.ElementRef<'a'>,
   React.ComponentPropsWithoutRef<'a'> & { icon: React.ElementType }
->(({ className, title, children, icon: Icon, ...props }, ref) => {
+>(({ className, title, children, icon: Icon, href, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
+          href={href!}
           ref={ref}
           className={cn(
             'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
@@ -257,10 +258,9 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   );
 });
 ListItem.displayName = 'ListItem';
-
