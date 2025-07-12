004800e8744b936b15f8ca8b48146aa93269d949
Zeta Leyva <zetaleyva2045@gmail.com>
2025-07-12 21:15:12 +0000

    Ahora agregaremos una seccion para personalizacion de regalo. "Personali

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Footer } from "@/components/anella/Footer";
 import { GiftRecommendation } from "@/components/anella/GiftRecommendation";
 import { GiftGallery } from "@/components/anella/GiftGallery";
 import { HeroSection } from "@/components/anella/HeroSection";
+import { GiftPersonalization } from "@/components/anella/GiftPersonalization";
 
 export default function Home() {
   return (
@@ -10,6 +11,8 @@ export default function Home() {
       <Header />
       <main className="flex-grow">
         <HeroSection />
+
+        <GiftPersonalization />
 
         <GiftGallery />
 
