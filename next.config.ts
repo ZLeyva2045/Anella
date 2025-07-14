6165db4686b5ce11db2f4013a5f208dcb9a5e017
Zeta Leyva <zetaleyva2045@gmail.com>
2025-07-14 02:48:00 +0000

    Error: Invalid src prop (https://metroio.vtexassets.com/arquivos/ids/241

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'metroio.vtexassets.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
