f756997c9f93ac7c9bf02a953bc5de7f05a54030
Zeta Leyva <zetaleyva2045@gmail.com>
2025-07-21 14:48:29 +0000

    Aun asi sigue rota la imagen

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: "anella-boutique.firebasestorage.app", // Forzar el nombre de bucket correcto
+  storageBucket: "anella-boutique.appspot.com", // Corregir el nombre de bucket
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
